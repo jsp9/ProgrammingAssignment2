e35b64c6d9f6b2aba18aafd7489b7c23b91c2faa
jsp9 <jsp920325@gmail.com>
2017-06-14 12:54:31 -0500

    Create cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,5 @@
+##This first function creates a vector that contains the functions to create the inverse of a matrix. It saves the result in another element of the list
+
 makeCacheMatrix <- function(x = matrix()) { 
       inv <- NULL                            
       set <- function(y) {                   
@@ -13,7 +15,8 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-
+#If the inverse of the matrix has been calculated previously, and it has not changed, cacheSolve uses the function get inverse from above
+#And returns the inverse previously calculated. Conversely, if it hasn't, it calculates the inverse.
   cacheSolve <- function(x, ...) {
         inv <- x$getinverse()
         if(!is.null(inv)) {
@@ -26,7 +29,8 @@ makeCacheMatrix <- function(x = matrix()) {
        inv
   }
   
-  
+  ##Testing the function.
+
   tau <- c(0, 0, 0, 0, 1, 
            1, 0, 0, 0, 0, 
            0, 1, 0, 0, 0, 
